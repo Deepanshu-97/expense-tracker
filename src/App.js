640ed3d7815bef760096b2d3e44cae04d4f0b9d7
Deepanshu Demla <deep.demla97@gmail.com>
2021-07-28 17:09:07 +0530

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { GlobalProvider } from "./context/GlobalState";
+import { Header } from "./components/Header";
 
 const App = () => {
   return (
     <GlobalProvider>
+      <Header />
     </GlobalProvider>
   );
-}
+};
 
 export default App;
