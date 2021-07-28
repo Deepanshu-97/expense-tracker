33071753f7d290a9ae0961e40b37d2eddc8a7611
Deepanshu Demla <deep.demla97@gmail.com>
2021-07-28 17:44:51 +0530

    Added transaction list and transaction component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { GlobalProvider } from "./context/GlobalState";
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import { AddTransaction } from "./components/AddTransaction";
+import { TransactionList } from "./components/TransactionList";
 import "./App.css";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <div className="container">
         <Balance />
         <AddTransaction />
+        <TransactionList />
       </div>
     </GlobalProvider>
   );
