91415db1d3e5c4acc544017350a7181206705268
Deepanshu Demla <deep.demla97@gmail.com>
2021-07-28 17:18:06 +0530

    Added balance component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { GlobalProvider } from "./context/GlobalState";
 import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
 
 const App = () => {
   return (
     <GlobalProvider>
       <Header />
+      <div className="container">
+        <Balance />
+      </div>
     </GlobalProvider>
   );
 };
